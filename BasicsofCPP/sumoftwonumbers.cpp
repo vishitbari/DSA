022df56489ec8c7602fa63185f38d8e88ac93c8f
Vishit Bari <barivishit28@gmail.com>
2025-09-11 15:46:29 +0530

    Create sumoftwonumbers.cpp

---
+++ BasicsofCPP/sumoftwonumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+    cout<<"Enter number 1 : ";
+    cin>>num1;
+    cout<<"Enter number 2 : ";
+    cin>>num2;
+
+    sum = num1 + num2;
+    cout<<"The sum of the two numbers is "<<sum;
+    }
