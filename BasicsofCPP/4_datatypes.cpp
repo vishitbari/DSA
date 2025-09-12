182e5ffd604927eb5fc86d209c15d8af448fc2fa
Vishit Bari <barivishit28@gmail.com>
2025-09-12 14:26:01 +0530

    Create 4_datatypes.cpp

---
+++ BasicsofCPP/4_datatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int fav_num;
+    fav_num = 8;
+    cout << "My Favorite number is " << fav_num << endl;
+
+    string name;
+    name = "Vishit Bari";
+    cout << "My name is " << name << endl;
+
+    float num1 = 8.22;
+    double num2 = 8.22222222;
+    cout << "Float number: " << num1 << endl;
+    cout << "Double number: " << num2 << endl;
+
+    bool is_raining = false;
+    cout << "Is it raining? " << is_raining << endl;
+    return 0;
+}
